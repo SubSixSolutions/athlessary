9bc83286523f44b97a8a37468d7ad45261feee9d
MaxMcCarthy <mamccrt2@illinois.edu>
2018-01-25 19:28:16 -0600

    Web Template Form

--- athlessary.py
+++ athlessary.py
@@ -3,8 +3,15 @@ import sqlite3
 import hashlib, uuid
 from flask import Flask, render_template, request, redirect, url_for, send_file, flash, jsonify
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
-
+#from flask_wtf import FlaskForm, Form, validators
+from wtforms import StringField, TextField, RadioField, IntegerField, SelectField, SubmitField, PasswordField, \
+    BooleanField
+from wtforms.validators import DataRequired, InputRequired, Length
 from User.user import User
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+
+
+
 
 app = Flask(__name__)
 app.secret_key = 'super secret string'  # Change this!
@@ -33,10 +40,41 @@ def create_connection(db_file):
 conn = create_connection('athlessary-database.db')
 
 
+class RegistrationForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
+
+
 @login_manager.user_loader
 def load_user(user_id):
     cur = conn.cursor()
     return User(user_id, cur)
+
+
+class MyForm(Form):
+    username = StringField('username', validators=[Length(min=1, max=5, message="must be less than 5 chars"), InputRequired("must not be empty")])
+    password = PasswordField('password')
+    address = StringField('address')
+    first = StringField('first name')
+    last = StringField('last name')
+    has_car = BooleanField('do you have a car?')
+    num_seats = IntegerField('num seats')
+
+
+@app.route('/login2', methods=['GET', 'POST'])
+def login2():
+    form = MyForm(request.form)
+    if request.method == 'POST' and form.validate():
+        print(form.username.data)
+        print(form['username'])
+        return 'hi'
+    return render_template('login2.html', form=form)
 
 
 @app.route('/login', methods=['POST', 'GET'])
