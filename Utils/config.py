ca81b893ede75a6aa89c561a9617e9f63ea7adc1
William Klock <william.klock@gmail.com>
2018-04-25 17:08:47 -0500

    Added configuration logging

--- Utils/config.py
+++ Utils/config.py
@@ -1,9 +1,10 @@
-import os, sys
+import os
+import sys
 
 from Utils.db import Database
+from Utils.log import log
 
 TESTING = bool(os.environ.get('TESTING'))
-DB_INIT = bool(os.environ.get('REQ_DB_INIT'))
 
 db = Database(TESTING)
 
@@ -11,11 +12,17 @@ try:
     twilio_sid = os.environ['TWILIO_SID']
     twilio_auth_token = os.environ['TWILIO_AUTH_TOKEN']
     twilio_number = os.environ['TWILIO_NUMBER']
-
 except KeyError:
     try:
         from Utils.secret_config import twilio_sid, twilio_auth_token, twilio_number
 
     except ModuleNotFoundError:
-        sys.stderr.write('Could Not Find Twilio Credentials')
+        log.error('Could Not Find Twilio Credentials')
         sys.exit(1)
+
+try:
+    DB_INIT = os.environ['REQ_DB_INIT']
+except KeyError:
+    DB_INIT = False
+
+log.info('DB_INIT: {}\nTESTING: {}\n')
